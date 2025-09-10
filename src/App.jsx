f58d1737c3104e60593c5a44608b66c9ce059da8
assignment-01 <ykhan9463@gmail.com>
2025-09-10 09:43:10 +0600

    Responsive Navbar using react and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
 import "./App.css";
+import DaisyNav from "./Components/DaisyNav";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <>
+      {/* <DaisyNav /> */}
+      <NavBar />
       <h1 className="text-3xl font-bold p-6 bg-amber-500 text-center">
         Price Options
       </h1>
-      {/* You can open the modal using document.getElementById('ID').showModal() method */}
-      <button
-        className="btn"
-        onClick={() => document.getElementById("my_modal_3").showModal()}
-      >
-        open modal
-      </button>
-      <dialog id="my_modal_3" className="modal">
-        <div className="modal-box">
-          <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-              ✕
-            </button>
-          </form>
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Press ESC key or click on ✕ button to close</p>
-        </div>
-      </dialog>
     </>
   );
 }
 
 export default App;
-        
